409e8b01d2094359750d610549b6ac7167443ec2
vishaldhateria <vishaldhateria@gmail.com>
2017-03-28 16:08:50 -0500

    updated

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 require 'connection.php';
 function checkuser($fuid,$ffname,$femail){
-    	$check = mysql_query("select * from users_facebook where Fuid='$fuid'");
-	$check = mysql_num_rows($check);
+    	$check = mysqli_query("select * from users_facebook where Fuid='$fuid'");
+	$check = mysqli_num_rows($check);
 	if (empty($check)) { // if new user . Insert a new record		
 	$query = "INSERT INTO users_facebook (Fuid,Ffname,Femail) VALUES ('$fuid','$ffname','$femail')";
-	mysql_query($query);	
+	mysqli_query($query);	
 	} else {   // If Returned user . update the user record		
 	$query = "UPDATE users_facebook SET Ffname='$ffname', Femail='$femail' where Fuid='$fuid'";
-	mysql_query($query);
+	mysqli_query($query);
 	}
 }?>
